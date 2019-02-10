f5a7a2b29c39385f149ba86ffddcedc45a9ecb60
wil26 <wil26@DESKTOP-AGDSCAF.localdomain>
2019-02-10 12:11:25 -0800

    added algorithm for file transfer

--- server.cpp
+++ server.cpp
@@ -71,56 +71,74 @@ int main(int argc, char * argv[])
 	buffer = malloc(PCKLEN);//creating buffer for maximum packet length
 //recieve connection
 	
-	while(1){
 		//will be the file request
-		n = recvfrom(sock, buffer, PCKLEN, 0, (struct sockaddr*) &client_addr, &clisize);
-		if(n < 0)
-			error_msg("Recieve Failed");
-		else
-			cout<<"Recieved"<<endl;
+	n = recvfrom(sock, buffer, PCKLEN, 0, (struct sockaddr*) &client_addr, &clisize);
+	if(n < 0)
+		error_msg("Recieve Failed");
+	else
+		cout<<"Recieved"<<endl;
 	//	cout<<"buffer: "<<(char *)buffer<<endl;//casts the buffer to void *, shows the "filename" we recieved from client
 		
 		//check if we have the file in question
-		if(access((char *)buffer, F_OK) == -1){
-			error_msg("We do not have that file.");
-		}
-		else{
-			cout<<"File has been found!" <<endl;
-		}
+	if(access((char *)buffer, F_OK) == -1){
+		error_msg("We do not have that file.");
+	}
+	else{
+		cout<<"File has been found!" <<endl;
+	}
 		
 		
 		//need to send packet with file size
-		FILE * file = fopen((char *)buffer, "r");
+	FILE * file = fopen((char *)buffer, "r");
+
+
 		//Gets the size of the file we want
-		struct stat file_stat;
-		stat((char*)buffer, &file_stat);
-		filesize = file_stat.st_size;
+	struct stat file_stat;
+	stat((char*)buffer, &file_stat);
+	filesize = file_stat.st_size;
+	cout<<"file size = " <<filesize<<endl;
 
-		cout<<"file size = " <<filesize<<endl;
-		void * data = malloc(PCKLEN);
-		memcpy(data, &filesize, sizeof(int));//copies the filesize into the pointer to be sent as data	
-		send_ack_packet(sock, SYN_ACK, 2, data, client_addr, clisize);//sequence number of 2 since we've already recv'd reguest
-		cout<<"Ack sent"<<endl;
 
+	//create pointer to point to the file size so that we may pass this back to the client in our SYN_ACK
+	void * data = malloc(PCKLEN);
+	memcpy(data, &filesize, sizeof(int));//copies the filesize into the pointer to be sent as data	
+	send_ack_packet(sock, SYN_ACK, 2, data, client_addr, clisize);//sequence number of 2 since we've already recv'd reguest
+	cout<<"Ack sent"<<endl;//confirmation we sent the ACK.
+
+			
+	//recv the ack from client before transmitting file. COMMENTED OUT THE RECVFROM AND DESERIALIZE FOR TIME BEING
+	void * ack_to_begin = malloc(PCKLEN);
+//	recvfrom(sock, ack_to_begin, PCKLEN, 0, (struct sockaddr*) &client_addr, &clisize);//recieve ack packet.
+	packet beginpacket;
+//	beginpacket.deserialize(ack_to_begin);
+
+	
+	
+	int totalbytes =0;//total bytes we have sent to client. will let us know when to close the connection.
+	int total_packets_to_send = filesize /1024;//the number of packets we will be sending.
+	cout<<"total packets to be sent" <<total_packets_to_send<<endl;//I just wanted to see how many.
+			
+	//to loop while we transfer the files. 
+	while(1){
+		//read from the file. Read into data with size PCKLEN(1024) bytes, up to the total_packets_to_send number of elements.	
+		totalbytes = fread(data, PCKLEN,total_packets_to_send, file);
 		
-
+		//after we have read, we can send the packet
 		
-	//recv the ack from client before transmitting.
+		//start our timer
 
-	//	packet mypack;
-	//	mypack.deserialize(buffer);
+		//wait for recieving ACK
+		
+		//when we recieve ACK, cancel the timer. 
+
+		//check that the sequence numbers match
+
+		//if they do not, resend our packet, if they do, start over. 
 	
 	}//end of while(1) for sending and recv packets. 
 	
 
 	close(sock);
-//reply once we get connection request 
-//recieve connection ack
-
-//then start sending files.
-
-
-	
 
 	return 0;
 }
@@ -130,11 +148,6 @@ void error_msg(const char * message)
 	perror(message);
 	exit(1);
 }
-/*	
-int myaccept(int sockid, struct sockaddr * clientaddr, socklen_t clientlen)
-{
-	return 0;
-}*/
 
 void send_ack_packet(int sockID, packettype type, int sequence_num, void * data, sockaddr_in client_socket,
  			socklen_t clilen)
